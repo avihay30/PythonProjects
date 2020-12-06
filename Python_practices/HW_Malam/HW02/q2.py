6890a0841c032946471a30dd0cbec564f333c740
Avihay-Laptop <avihay30@gmail.com>
2020-12-06 18:44:50 +0200

    minor fix

--- Python_practices/HW_Malam/HW02/q2.py
+++ Python_practices/HW_Malam/HW02/q2.py
@@ -1,14 +1,14 @@
 import math
 
 
-def deleting_leftest_number(number, number_of_digits):
-    module_divider = int("1" + "0"*(number_of_digits - 1))
+def deleting_leftest_number(number):
+    # number_of_digits = int(math.log10(number)) + 1
+    module_divider = int("1" + "0" * (len(str(number)) - 1))
     return number % module_divider
 
 
-def get_leftest_digit(number, number_of_digits):
-    divider = int("1" + "0"*(number_of_digits - 1))
-    return number // divider
+def get_leftest_digit(number):
+    return int(str(number)[0])
 
 
 def add_unit_digit_to_number(number, digit):
@@ -19,13 +19,12 @@ def left_circular_shift(list):
     shifted_list = []
     leftest_of_first_element = 0
     for i in range(len(list)):
-        number_of_digits = int(math.log10(list[i])) + 1
-        leftest_digit = get_leftest_digit(list[i], number_of_digits)
+        leftest_digit = get_leftest_digit(list[i])
         if i == 0:
             leftest_of_first_element = leftest_digit
         if i != 0:
             shifted_list[i - 1] = add_unit_digit_to_number(shifted_list[i - 1], leftest_digit)
-        shifted_element = deleting_leftest_number(list[i], number_of_digits)
+        shifted_element = deleting_leftest_number(list[i])
         shifted_list.insert(i, shifted_element)
     shifted_list[-1] = add_unit_digit_to_number(shifted_list[-1], leftest_of_first_element)
     return shifted_list
@@ -34,7 +33,7 @@ def left_circular_shift(list):
 number = int(input("Enter number of elements: "))
 original_array = []
 for i in range(number):
-    original_array.append(int(input("Enter element %s: " % i)))
+    original_array.append(int(input("Enter element %s: " % (i + 1))))
 
 print("Original array: \n%s" % original_array)
 print("Shifted array: \n%s" % left_circular_shift(original_array))
